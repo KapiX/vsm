e8bad9cdf03a4e1b970e3154f49fc629d074e924
milkoc <milkoc@users.noreply.github.com>
2016-10-14 01:18:01 +0200

    sprinty baza danych

--- db/insert_example_data.sql
+++ db/insert_example_data.sql
@@ -25,14 +25,14 @@ INSERT INTO projects_users (id, project_id, user_id) VALUES (7,3,3);
 
 -- INSERT SCRUM_REPORTS
 -- p1
-INSERT INTO scrum_reports (id, project_id, deadline_date) VALUES (1,1,'2016-09-06 19:25:29');
-INSERT INTO scrum_reports (id, project_id, deadline_date) VALUES (2,1,'2016-09-13 19:25:29');
-INSERT INTO scrum_reports (id, project_id, deadline_date) VALUES (3,1,'2016-09-20 19:25:29');
-INSERT INTO scrum_reports (id, project_id, deadline_date) VALUES (4,1,'2016-09-27 19:25:29');
+INSERT INTO scrum_reports (id, sprint_id, deadline_date) VALUES (1,1,'2016-09-06 19:25:29');
+INSERT INTO scrum_reports (id, sprint_id, deadline_date) VALUES (2,1,'2016-09-13 19:25:29');
+INSERT INTO scrum_reports (id, sprint_id, deadline_date) VALUES (3,1,'2016-09-20 19:25:29');
+INSERT INTO scrum_reports (id, sprint_id, deadline_date) VALUES (4,1,'2016-09-27 19:25:29');
 -- p2
-INSERT INTO scrum_reports (id, project_id, deadline_date) VALUES (5,2,'2016-09-26 19:25:29');
+INSERT INTO scrum_reports (id, sprint_id, deadline_date) VALUES (5,2,'2016-09-26 19:25:29');
 -- p3
-INSERT INTO scrum_reports (id, project_id, deadline_date) VALUES (6,3,'2016-09-14 19:25:29');
+INSERT INTO scrum_reports (id, sprint_id, deadline_date) VALUES (6,3,'2016-09-14 19:25:29');
 
 -- INSERT USER_SCRUM_REPORTS
 INSERT INTO user_scrum_reports (id, scrum_report_id, user_id, created, modified, q1_ans, q2_ans, q3_ans)
@@ -41,12 +41,12 @@ INSERT INTO user_scrum_reports (id, scrum_report_id, user_id, created, modified,
     VALUES (2,1,2,'2016-09-04 17:25:29','2016-09-04 18:25:29', '...', '...', '...');
 
 -- INSERT PROJECT_VSM_SETTINGS
-INSERT INTO project_vsm_settings (id, project_id, vsm_start_date, vsm_end_date, report_weekdays, report_overdue_notification_text, report_overdue_frequency_hours)
-    VALUES (1,1,'2016-09-01 19:25:29', '2016-11-01 19:25:29', '1','???',5);
-INSERT INTO project_vsm_settings (id, project_id, vsm_start_date, vsm_end_date, report_weekdays, report_overdue_notification_text, report_overdue_frequency_hours)
-    VALUES (2,2,'2016-09-01 19:25:29', '2016-11-01 19:25:29', '0','???',5);
-INSERT INTO project_vsm_settings (id, project_id, vsm_start_date, vsm_end_date, report_weekdays, report_overdue_notification_text, report_overdue_frequency_hours)
-    VALUES (3,3,'2016-09-01 19:25:29', '2016-11-01 19:25:29', '2','???',5);
+INSERT INTO project_vsm_settings (id, project_id, report_overdue_notification_text, report_overdue_frequency_hours)
+    VALUES (1,1,'???',5);
+INSERT INTO project_vsm_settings (id, project_id, report_overdue_notification_text, report_overdue_frequency_hours)
+    VALUES (2,2,'???',5);
+INSERT INTO project_vsm_settings (id, project_id, report_overdue_notification_text, report_overdue_frequency_hours)
+    VALUES (3,3,'???',5);
 
 -- INSERT USER_VSM_SETTINGS
 INSERT INTO user_vsm_settings (id, user_id)
@@ -55,3 +55,29 @@ INSERT INTO user_vsm_settings (id, user_id)
     VALUES (2,2);
 INSERT INTO user_vsm_settings (id, user_id)
     VALUES (3,3);
+	
+-- INSERT SPRINTS
+INSERT INTO sprints (id, project_id, name, start_date, end_date)
+    VALUES (1,1, 'sprint 1', '2016-09-01 19:25:29', '2016-09-15 19:25:29');
+INSERT INTO sprints (id, project_id, name, start_date, end_date)
+    VALUES (2,2, 'sprint 1', '2016-09-01 19:25:29', '2016-09-15 19:25:29');
+INSERT INTO sprints (id, project_id, name, start_date, end_date)
+    VALUES (3,3, 'sprint 1', '2016-09-01 19:25:29', '2016-09-15 19:25:29');
+	
+-- INSERT SPRINT_VSM_SETTINGS
+INSERT INTO sprint_vsm_settings (id, sprint_id, report_weekdays)
+    VALUES (1,1,'1,3,5');
+INSERT INTO sprint_vsm_settings (id, sprint_id, report_weekdays)
+    VALUES (2,2,'1,4');
+INSERT INTO sprint_vsm_settings (id, sprint_id, report_weekdays)
+    VALUES (3,3,'1,3,5');
+	
+-- INSERT SPRINTS_USERS
+INSERT INTO sprints_users (id, sprint_id, user_id)
+    VALUES (1,1,1);
+INSERT INTO sprints_users (id, sprint_id, user_id)
+    VALUES (1,1,2);
+INSERT INTO sprints_users (id, sprint_id, user_id)
+    VALUES (1,2,2);
+INSERT INTO sprints_users (id, sprint_id, user_id)
+    VALUES (1,3,3);
--- db/vsm_standalone_database.sql
+++ db/vsm_standalone_database.sql
@@ -21,7 +21,7 @@ CREATE TABLE IF NOT EXISTS `projects_users` (
 DROP TABLE IF EXISTS `scrum_reports`;
 CREATE TABLE IF NOT EXISTS `scrum_reports` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `project_id` int(11) NOT NULL,
+  `sprint_id` int(11) NOT NULL,
   `deadline_date` datetime,
   PRIMARY KEY (`id`)
 );
@@ -52,9 +52,6 @@ DROP TABLE IF EXISTS `project_vsm_settings`;
 CREATE TABLE IF NOT EXISTS `project_vsm_settings` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `project_id` int(11) NOT NULL,
-  `vsm_start_date` datetime,
-  `vsm_end_date` datetime,
-  `report_weekdays` varchar(255) NOT NULL,
   `report_overdue_notification_text` TEXT NOT NULL,
   `report_overdue_frequency_hours` int(11) NOT NULL,
   PRIMARY KEY (`id`)
@@ -63,6 +60,32 @@ CREATE TABLE IF NOT EXISTS `project_vsm_settings` (
 DROP TABLE IF EXISTS `user_vsm_settings`;
 CREATE TABLE IF NOT EXISTS `user_vsm_settings` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+DROP TABLE IF EXISTS `sprints`;
+CREATE TABLE IF NOT EXISTS `sprints` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `project_id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `start_date` datetime,
+  `end_date` datetime,
+  PRIMARY KEY (`id`)
+);
+
+DROP TABLE IF EXISTS `sprint_vsm_settings`;
+CREATE TABLE IF NOT EXISTS `user_vsm_settings` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `sprint_id` int(11) NOT NULL,
+  `report_weekdays` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+DROP TABLE IF EXISTS `sprints_users`;
+CREATE TABLE IF NOT EXISTS `projects_users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `sprint_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   PRIMARY KEY (`id`)
 );
